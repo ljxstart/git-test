824cc53b7b6eed59d5d81258f1810c4646478c96
ljx <ljxstart@126.com>
2023-07-25 15:54:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ *
+ * @Author lvjiaxun
+ * @Create 2023/7/25 15:48
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
